c3d954e198fe3c4fceca3dcba42de9705d7da8b6
Ricardo <ricardo.ortega97@outlook.com>
2024-03-09 00:13:05 -0500

    Initial Commit

---
+++ My Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Contact List
+//
+//  Created by Ricardo Ortega on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
